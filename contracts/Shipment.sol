8ba38db980e9b5dc378947dab3166c13d1a43c9a
sdi160068 <sdi1600068@di.uoa.gr>
2024-07-27 19:03:37 +0300

    Co-authored-by: thpap13 <thpap13@users.noreply.github.com>

--- contracts/Shipment.sol
+++ contracts/Shipment.sol
@@ -36,9 +36,8 @@ contract Pharmacy {
     mapping(address => User) public users;
     mapping(address => uint256) public scEntities_index;
     mapping(uint256 => uint256) public products_index;
-
-    mapping(address => Product[]) public supplier_products;
-    mapping(address => Product[]) public logistic_products;
+    mapping(address => uint256[]) public entities_products;
+    mapping(uint256 => uint256[]) public products_shipments;
 
     uint256 products_ids = 1;
 
@@ -75,17 +74,64 @@ contract Pharmacy {
         });
         users[msg.sender] = newUser;
 
-        address addr = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-        create_ScEntity(addr,"Supplier 0", EntityType.Supplier);
-        create_ScEntity(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db,"Transportation 1", EntityType.Transportation);
-        create_ScEntity(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB,"Warehouse_Logistic 3", EntityType.Warehouse_Logistic);
+        address supplier_address_1 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+        create_ScEntity(supplier_address_1,"Supplier 0", EntityType.Supplier);
+        create_user("Supplier 0", Role.Supplier, supplier_address_1);
 
-        create_product("Depon", 10 , addr);
-        create_product("Ponstan", 10 , addr);
-        create_product("Amoxil", 10 , addr);
+        address supplier_address_2 = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+        create_ScEntity(supplier_address_2,"Supplier 1", EntityType.Supplier);
+        create_user("Supplier 1", Role.Supplier, supplier_address_2);
+
+        address transportation_address = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
+        create_ScEntity(transportation_address,"Transportation 1", EntityType.Transportation);
+
+        address manufacturer_address = 0x617F2E2fD72FD9D5503197092aC168c91465E7f2;
+        create_ScEntity(manufacturer_address,"Manufacturer 0", EntityType.Manufacturer);
+        
+        address logisitc_address_1 = 0x17F6AD8Ef982297579C203069C1DbfFE4348c372;
+        create_ScEntity(logisitc_address_1,"Warehouse_Logistic 0", EntityType.Warehouse_Logistic);
+        create_user("Logistic 0", Role.Logistic, logisitc_address_1);
+
+        address logisitc_address_2 = 0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678;
+        create_ScEntity(logisitc_address_2,"Warehouse_Logistic 1", EntityType.Warehouse_Logistic);
+        create_user("Logistic 1", Role.Logistic, logisitc_address_2);
+
+        address distributor_address = 0x03C6FcED478cBbC9a4FAB34eF9f40767739D1Ff7;
+        create_ScEntity(distributor_address,"Distributor 0", EntityType.Distributor);
+
+        address pharmacy_address = 0x1aE0EA34a72D944a8C7603FfB3eC30a6669E454C;
+        create_ScEntity(pharmacy_address,"Pharmacy 0", EntityType.Pharmacy);
+
+        address auditor_address = 0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC;
+        create_user("Auditor 1", Role.Auditor, auditor_address);
+
+        create_product("Panadol", 10, supplier_address_1);
+        create_product("Depon", 10, supplier_address_1);
+        create_product("Algofren", 10, supplier_address_2);
+        create_product("Aspirin", 10, supplier_address_1);
+        create_product("Augmentin", 10, supplier_address_2);
+        create_product("Otrivin", 10, supplier_address_1);
+        create_product("Voltaren", 10, supplier_address_2);
+        create_product("Ponstan", 10, supplier_address_1);
+        create_product("Fenistil", 10, supplier_address_2);
+        create_product("Betadine", 10, supplier_address_1);
+
+
+        // function create_shipment(address destination, uint256 product_id, uint256 expected_date_of_arrival) public {
+        for(uint256 product_id = 1; product_id < 11; product_id ++){
+            create_shipment(transportation_address , product_id , 1729087731);
+            create_shipment(manufacturer_address , product_id , 1729087731);
+            if(product_id % 2 == 1){ 
+                create_shipment(logisitc_address_1 , product_id , 1729087731);
+            }
+            else{
+                create_shipment(logisitc_address_2 , product_id , 1729087731);
+            }
+            create_shipment(distributor_address , product_id , 1729087731);
+            create_shipment(pharmacy_address , product_id , 1729087731);
+        }
     }
-
-    // User Management
+                    // User Management
     function create_user(string memory name, Role role, address user_address) public only_for_role(Role.Admin) {
         require(users[user_address].user_address != user_address, "User already exists");
         User memory newUser = User({
@@ -135,13 +181,17 @@ contract Pharmacy {
             product_id: product_id
         });
 
+        // Save shipment to list for this product
+        products_shipments[product_id].push(newShipment.shipment_id);
+
         // Adds product_id to Logistic employee (if entity is a Logistic warehouse )
-        if( scEntities_list[destination_index].entity_type == Role.Logistic ){
-            logistic_products[destination].push(product_id);
+        if( scEntities_list[destination_index].entity_type == EntityType.Warehouse_Logistic ){
+            entities_products[destination].push(product_id);
         }
 
         products_list[p_index].currentScEntity = destination;
-        shipments_list.push(newShipment);
+        shipments_list.push(newShipment);                    
+
         emit ShipmentCreated(newShipment.shipment_id, newShipment.origin, newShipment.destination, newShipment.date_of_departure, newShipment.expected_date_of_arrival,  newShipment.product_id);
     }
 
@@ -155,6 +205,44 @@ contract Pharmacy {
         return shipments_list;
     }
 
+    function get_product_shipments(uint256 product_id) public view returns (Shipment[] memory) {
+        require(products_index[product_id] != 0 || products_list[0].id == product_id, "Product does not exist!");
+
+        User memory user = users[msg.sender];
+        require(user.user_address == msg.sender, "Not authorized");
+
+        uint256[] memory shipment_ids = products_shipments[product_id];
+
+        if( user.role == Role.Supplier || user.role == Role.Logistic){
+            bool product_belongs_to_user = false;
+            uint256[] memory user_products = entities_products[msg.sender];
+            for (uint256 i = 0; i < user_products.length; i++) {
+                if (user_products[i] == product_id) {
+                    product_belongs_to_user = true;
+                    break;
+                }
+            }
+            require(product_belongs_to_user, "Product does not belong to the user");
+        
+            uint256 max_shipments = user.role == Role.Supplier ? 2 : 3;
+            uint256 shipment_count = shipment_ids.length < max_shipments ? shipment_ids.length : max_shipments;
+
+            Shipment[] memory _shipments = new Shipment[](shipment_count);
+            for (uint256 i = 0; i < shipment_count; i++) {
+                _shipments[i] = shipments_list[shipment_ids[i]];
+            }
+
+            return _shipments;
+        }
+
+        Shipment[] memory shipments = new Shipment[](shipment_ids.length);
+        for (uint256 i = 0; i < shipment_ids.length; i++) {
+            shipments[i] = shipments_list[shipment_ids[i]];
+        }
+
+        return shipments;
+    }
+
     function set_expected_date_of_arrival(uint256 shipment_id) public {
         shipments_list[shipment_id].expected_date_of_arrival = block.timestamp;
     }
@@ -163,7 +251,7 @@ contract Pharmacy {
     function create_product(string memory name, uint256 quantity, address currentScEntity) public {
         uint256 index = scEntities_index[currentScEntity];
         require(index < scEntities_list.length && scEntities_list[index].entity_address == currentScEntity, "Cannot create product because Entity does not exist!");
-        require(scEntities_list[index].entity_type == Role.Supplier, "Cannot create product because Entity is not a Supplier!");
+        require(scEntities_list[index].entity_type == EntityType.Supplier, "Cannot create product because Entity is not a Supplier!");
 
         Product memory newProduct = Product({
             id: products_ids++,
@@ -176,7 +264,7 @@ contract Pharmacy {
         products_list.push(newProduct);
 
         // adds product to supplier
-        supplier_products[currentScEntity].push(newProduct.id);
+        entities_products[currentScEntity].push(newProduct.id);
 
         emit ProductCreated(newProduct.id, newProduct.name, newProduct.quantity, newProduct.currentScEntity);
     }
@@ -209,11 +297,14 @@ contract Pharmacy {
     }
 
     function get_products() public view returns (Product[] memory) {
-        if( users[msg.sender].role == Role.Supplier ){
-            return supplier_products[msg.sender];
-        }
-        else if(users[msg.sender].role == Role.Logistic){
-            return logistic_products[msg.sender];
+        if( users[msg.sender].role == Role.Supplier || users[msg.sender].role == Role.Logistic){
+            Product[] memory _products_list = new Product[](entities_products[msg.sender].length);
+
+            for(uint256 i=0; i < entities_products[msg.sender].length ; i++ ){
+                _products_list[i] = products_list[products_index[entities_products[msg.sender][i]]];
+            }
+
+            return _products_list;
         }
         return products_list;
     }
